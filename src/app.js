11095ca74ef12c40b57c84cde2f2d23dc1953041
Aidan O'Malley <aidanomalley@Aidans-MacBook-Pro.local>
2024-02-19 12:38:58 -0800

    try to call api endpoint

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
-    <div>
-        yo my name is Aidan, I have a cat named oopsie
-    </div>
-);
+const App = () => {
+
+    const [response, setResponse] = React.useState('');
+
+    React.useEffect(() => {
+        fetch('https://api.spider2ybanana.com/')
+            .then(res => res.json())
+            .then(res => console.log(res) || setResponse(res));
+    }, [])
+
+    return (
+        <div>
+            yo my name is Aidan, I have a cat named oopsie
+
+            {JSON.stringify(response)}
+        </div>
+    );
+};
 
 ReactDOM.render(<App/>, document.getElementById('root'));
